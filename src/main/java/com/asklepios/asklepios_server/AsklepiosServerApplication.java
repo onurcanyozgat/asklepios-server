f06fac86df366d1104034efdb4590d2a4c7afee3
onurcan.yozgat <onurcan.yozgat@esensi.com.tr>
2024-06-25 13:29:40 +0300

    first commit

---
+++ src/main/java/com/asklepios/asklepios_server/AsklepiosServerApplication.java
@@ -0,0 +1,13 @@
+package com.asklepios.asklepios_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsklepiosServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsklepiosServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asklepios/asklepios_server/AsklepiosServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asklepios.asklepios_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsklepiosServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
