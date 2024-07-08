d416a2310ea1839ddf771e8729f86993ccfcd48f
onurcan.yozgat <onurcan.yozgat@esensi.com.tr>
2024-07-08 18:01:10 +0300

    education career converters added

--- src/main/java/com/asklepios/asklepios_server/doctor/converter/CareerConverter.java
+++ src/main/java/com/asklepios/asklepios_server/doctor/converter/CareerConverter.java
@@ -8,11 +8,25 @@ import org.springframework.stereotype.Component;
 public class CareerConverter {
     public CareerDto toDto(CareerEntity entity) {
         CareerDto dto = new CareerDto();
+        dto.setId(entity.getId());
+        dto.setStartYear(entity.getStartYear());
+        dto.setEndYear(entity.getEndYear());
+        dto.setTitle(entity.getTitle());
+        dto.setName(entity.getName());
+        dto.setDescription(entity.getDescription());
+        dto.setCountry(entity.getCountry());
         return dto;
     }
 
     public CareerEntity toEntity(CareerDto dto) {
         CareerEntity entity = new CareerEntity();
+        entity.setId(dto.getId());
+        entity.setStartYear(dto.getStartYear());
+        entity.setEndYear(dto.getEndYear());
+        entity.setTitle(dto.getTitle());
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setCountry(dto.getCountry());
         return entity;
     }
 }
--- src/main/java/com/asklepios/asklepios_server/doctor/converter/EducationConverter.java
+++ src/main/java/com/asklepios/asklepios_server/doctor/converter/EducationConverter.java
@@ -8,11 +8,25 @@ import org.springframework.stereotype.Component;
 public class EducationConverter {
     public EducationDto toDto(EducationEntity entity) {
         EducationDto dto = new EducationDto();
+        dto.setId(entity.getId());
+        dto.setStartYear(entity.getStartYear());
+        dto.setGraduateYear(entity.getGraduateYear());
+        dto.setDegree(entity.getDegree());
+        dto.setDepartment(entity.getDepartment());
+        dto.setUniversity(entity.getUniversity());
+        dto.setCountry(entity.getCountry());
         return dto;
     }
 
     public EducationEntity toEntity(EducationDto dto) {
         EducationEntity entity = new EducationEntity();
+        entity.setId(dto.getId());
+        entity.setStartYear(dto.getStartYear());
+        entity.setGraduateYear(dto.getGraduateYear());
+        entity.setDegree(dto.getDegree());
+        entity.setDepartment(dto.getDepartment());
+        entity.setUniversity(dto.getUniversity());
+        entity.setCountry(dto.getCountry());
         return entity;
     }
 
