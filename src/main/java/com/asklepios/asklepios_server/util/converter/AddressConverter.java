8c7563acb2196a1ff337437ba25bf240bb1ca487
onurcan.yozgat <onurcan.yozgat@esensi.com.tr>
2024-07-08 18:04:11 +0300

    address and telephone number converters added

--- src/main/java/com/asklepios/asklepios_server/util/converter/AddressConverter.java
+++ src/main/java/com/asklepios/asklepios_server/util/converter/AddressConverter.java
@@ -8,11 +8,23 @@ import org.springframework.stereotype.Component;
 public class AddressConverter {
     public AddressDto toDto(AddressEntity entity) {
         AddressDto dto = new AddressDto();
+        dto.setId(entity.getId());
+        dto.setAddress(entity.getAddress());
+        dto.setCity(entity.getCity());
+        dto.setCountry(entity.getCountry());
+        dto.setLatitude(entity.getLatitude());
+        dto.setLongitude(entity.getLongitude());
         return dto;
     }
 
     public AddressEntity toEntity(AddressDto dto) {
         AddressEntity entity = new AddressEntity();
+        entity.setId(dto.getId());
+        entity.setAddress(dto.getAddress());
+        entity.setCity(dto.getCity());
+        entity.setCountry(dto.getCountry());
+        entity.setLatitude(dto.getLatitude());
+        entity.setLongitude(dto.getLongitude());
         return entity;
     }
 }
--- src/main/java/com/asklepios/asklepios_server/util/converter/TelephoneNumberConverter.java
+++ src/main/java/com/asklepios/asklepios_server/util/converter/TelephoneNumberConverter.java
@@ -1,8 +1,24 @@
 package com.asklepios.asklepios_server.util.converter;
 
+import com.asklepios.asklepios_server.util.data.dto.TelephoneNumberDto;
+import com.asklepios.asklepios_server.util.data.entity.TelephoneNumberEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TelephoneNumberConverter {
+    public TelephoneNumberDto toDto(TelephoneNumberEntity entity) {
+        TelephoneNumberDto dto = new TelephoneNumberDto();
+        dto.setId(entity.getId());
+        dto.setCountryCode(entity.getCountryCode());
+        dto.setNumber(entity.getNumber());
+        return dto;
+    }
 
+    public TelephoneNumberEntity toEntity(TelephoneNumberDto dto) {
+        TelephoneNumberEntity entity = new TelephoneNumberEntity();
+        entity.setId(dto.getId());
+        entity.setCountryCode(dto.getCountryCode());
+        entity.setNumber(entity.getNumber());
+        return entity;
+    }
 }
